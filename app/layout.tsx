19341d8eaa0bf32757127098cb205273e2943c21
devHong <choisieun89@gmail.com>
2023-10-12 10:51:20 +0900

    Add setting eslint & prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta key="charset" name="charset" content="utf-8" />
+        {/* FIXME: tailwind를 사용하는데 필요가 있나? */}
+        <meta key="viewpoint" name="viewpoint" />
+        <meta property="og:locale" content="ko_KR" />
+        <meta property="og:type" content="website" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
